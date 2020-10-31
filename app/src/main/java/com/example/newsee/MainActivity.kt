dd40de6dfdf918b8f12c158110505386ca44d2ba
Kazuya Izumi <shikibu9419@gmail.com>
2020-10-31 10:40:27 +0900

    Add OverlayView sample

--- app/src/main/java/com/example/newsee/MainActivity.kt
+++ app/src/main/java/com/example/newsee/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.newsee
 
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.WindowManager
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        /** ID for the runtime permission dialog */
+        private const val OVERLAY_PERMISSION_REQUEST_CODE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestOverlayPermission()
+
+        // Show/hide overlay view with a toggle button.
+        findViewById<ToggleButton>(R.id.toggle_button).apply {
+            isChecked = OverlayService.isActive
+            setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) OverlayService.start(this@MainActivity)
+                else OverlayService.stop(this@MainActivity)
+            }
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // ユーザから許可が得られなかったらアプリを終了させる
+        if (requestCode == OVERLAY_PERMISSION_REQUEST_CODE) {
+            if (!isOverlayGranted()) {
+                finish()  // Cannot continue if not granted
+            }
+        }
+    }
+
+    /* 必要に応じてユーザに権限をリクエスト */
+    private fun requestOverlayPermission() {
+        if (isOverlayGranted()) return
+        val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+        )
+        startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST_CODE)
+    }
+
+    /** オーバーレイの権限があるかどうかチェック */
+    private fun isOverlayGranted() =
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
+                    Settings.canDrawOverlays(this)
 }
--- app/src/main/java/com/example/newsee/OverlayService.kt
+++ app/src/main/java/com/example/newsee/OverlayService.kt
@@ -1,2 +1,69 @@
 package com.example.newsee
 
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+
+/**
+ * A foreground service for managing the life cycle of overlay view.
+ */
+class OverlayService : Service() {
+    companion object {
+        private const val ACTION_SHOW = "SHOW"
+        private const val ACTION_HIDE = "HIDE"
+
+        fun start(context: Context) {
+            val intent = Intent(context, OverlayService::class.java).apply {
+                action = ACTION_SHOW
+            }
+            Log.d("OverlayService", "start" + isActive)
+            context.startService(intent)
+        }
+
+        fun stop(context: Context) {
+            val intent = Intent(context, OverlayService::class.java).apply {
+                action = ACTION_HIDE
+            }
+            Log.d("OverlayService", "stop" + isActive)
+            context.startService(intent)
+        }
+
+        // To control toggle button in MainActivity. This is not elegant but works.
+        var isActive = false
+            private set
+    }
+
+    private lateinit var overlayView: OverlayView
+
+    override fun onCreate() {
+        overlayView = OverlayView.create(this)
+    }
+
+    /** Handles [ACTION_SHOW] and [ACTION_HIDE] intents. */
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("onStartCommand", "called")
+        intent?.let {
+            it.action?.let { it1 -> Log.d("intent.action", it1) }
+            when (it.action) {
+                ACTION_SHOW -> {
+                    isActive = true
+                    overlayView.show()
+                }
+                ACTION_HIDE -> {
+                    isActive = false
+                    overlayView.hide()
+                    stopSelf()
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    /** Cleans up views just in case. */
+    override fun onDestroy() = overlayView.hide()
+
+    /** This service does not support binding. */
+    override fun onBind(intent: Intent?) = null
+}
